d0bc7a2a5d0cf8e3da27c66b9e67549f44522520
Rayan <87863722+unonodes@users.noreply.github.com>
2022-12-09 13:53:38 +0200

    tfs_v0.19.0_000aec2_devnet
    tfs_v0.19.0_000aec2_devnet

--- transformers/tfsc_update.sh
+++ transformers/tfsc_update.sh
@@ -28,7 +28,7 @@ sleep 2
 
 echo -en '\n'
 echo "Скачивание бинарного файла"
-wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/tfs_v0.18.0_26a1149_devnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/tfs_v0.19.0_000aec2_devnet
 sleep 10
 
 echo -en '\n'
