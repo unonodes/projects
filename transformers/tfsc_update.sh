e2f62450015a4d3bfc299cb6bfb1f95f5b5d09f1
Rayan <87863722+unonodes@users.noreply.github.com>
2022-12-19 22:34:34 +0200

    Update v19.1
    Update v19.1

--- transformers/tfsc_update.sh
+++ transformers/tfsc_update.sh
@@ -28,7 +28,7 @@ sleep 2
 
 echo -en '\n'
 echo "Скачивание бинарного файла"
-wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/tfs_v0.19.0_000aec2_devnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/tfs_v0.19.1_a2b45a4_devnet
 sleep 10
 
 echo -en '\n'
